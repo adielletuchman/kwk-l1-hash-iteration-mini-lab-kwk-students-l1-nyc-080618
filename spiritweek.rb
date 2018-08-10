54a2b444c51999a7e01fbe63c6b0cb8758528367
adielletuchman <adielle.tuchman@mjbha.org>
2018-08-10 17:54:14 +0000

    automatically backed up by learn

--- spiritweek.rb
+++ spiritweek.rb
@@ -3,7 +3,7 @@
 
 # have it output "the spirit day for Monday is ____" etc.
 
-spirit_week_hash = {"Monday" => "PJ", "Tuesday" => "rep school", "Wednesday" => "wear pink", "Thursday" => "neon", "Friday" => "dress your best"}
+spirit_week_hash = {"Monday" => "PJ", "Tuesday" => "rep school", "Wednesday" => "wear pink", "Thursday" => "disney/movie", "Friday" => "fancy friday"}
 spirit_week_hash.each do |week, theme|
   puts "The spirit day for #{week} is #{theme} day!"
   
